31e1016e0291c62e6db6f9b817823ec0ba05baf6
Yosip Mike Colin <colinparrado@gmail.com>
2024-06-11 18:02:37 -0500

    Settings server

--- websocket.js
+++ websocket.js
@@ -1,49 +1,63 @@
 import express from "express";
-import { WebSocketServer } from "ws";
+import fs from "fs";
+import path from "path";
 import cors from "cors";
+import { fileURLToPath } from "url";
+
+// Definir __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // Inicia el servidor HTTP
-const server = app.listen(process.env.PORT || 4000, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log(`Server listening on port ${process.env.PORT || 4000}`);
 });
 
-// Configura el WebSocketServer utilizando el servidor HTTP
-const wss = new WebSocketServer({ server });
-
-wss.on("connection", (ws) => {
-  console.log("Client connected");
-
-  ws.on("message", (message) => {
-    console.log(`Received message => ${message}`);
-  });
-
-  ws.on("error", (error) => {
-    console.error("WebSocket error occurred", error);
-  });
-
-  ws.on("close", (code, reason) => {
-    console.log(`Client disconnected with code ${code}, reason: ${reason}`);
+app.post("/api/save-data", (req, res) => {
+  const { filename, data } = req.body;
+  if (!filename || !data) {
+    return res.status(400).send("Faltan el nombre del archivo o los datos");
+  }
+  const filePath = path.join(__dirname, "data", `${filename}.json`);
+  fs.writeFile(filePath, JSON.stringify(data, null, 2), (err) => {
+    if (err) {
+      return res.status(500).send("Error al guardar los datos");
+    }
+    return res.status(200).send("Datos guardados exitosamente");
   });
 });
 
-function broadcastMessage(data) {
-  wss.clients.forEach((client) => {
-    if (client.readyState === client.OPEN) {
-      client.send(JSON.stringify(data));
-    }
-  });
-}
+app.get("/api/get-data/:filename", (req, res) => {
+  const filename = req.params.filename;
 
-app.post("/api/webhook", (req, res) => {
-  console.log("Received POST request from another server");
-  const { message, id } = req.body;
-  if (message === "payment-success") {
-    console.log(`Broadcasting message: ${message} with id: ${id}`);
-    broadcastMessage({ message, id });
+  if (!filename) {
+    return res.status(400).send("Falta el nombre del archivo");
   }
-  res.json({ message: "OK" });
+
+  // Ruta del archivo específico
+  const filePath = path.join(__dirname, "data", `${filename}.json`);
+
+  // Leer el archivo
+  fs.readFile(filePath, "utf8", (err, fileData) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        return res.status(404).send("Archivo no encontrado");
+      }
+      return res.status(500).send("Error al leer el archivo");
+    }
+
+    // Devolver el contenido del archivo
+    res.status(200).json(JSON.parse(fileData));
+
+    // Eliminar el archivo después de devolver su contenido
+    fs.unlink(filePath, (unlinkErr) => {
+      if (unlinkErr) {
+        console.error("Error al eliminar el archivo:", unlinkErr);
+      }
+    });
+  });
 });
